68b35292d7b322c14d558d60f9395a7c09f98031
Marcelo Riss <marcelo.riss@animati.com.br>
2020-09-27 23:44:56 -0300

    optimizing import

--- clock-in-processor/backend/app/src/main/java/com/mriss/products/mailprocessorservice/clockin/app/config/RequestResponseLoggingFilter.java
+++ clock-in-processor/backend/app/src/main/java/com/mriss/products/mailprocessorservice/clockin/app/config/RequestResponseLoggingFilter.java
@@ -2,16 +2,7 @@ package com.mriss.products.mailprocessorservice.clockin.app.config;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Properties;
 
-import javax.mail.BodyPart;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -19,13 +10,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import com.mriss.products.mailprocessorservice.clockin.app.service.MailInfoExtractor;
 import com.mriss.products.mailprocessorservice.clockin.app.service.Validator;
